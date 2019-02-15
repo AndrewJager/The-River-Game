ecdee523874895d00856ff25540552c35cac9738
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2019-02-15 18:26:13 -0500

    Here we go

---
+++ main.lua
@@ -0,0 +1,15 @@
+--Big Fish references added: 0
+
+local world = {}
+
+function love.load()
+    love.window.setTitle("The River")
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
